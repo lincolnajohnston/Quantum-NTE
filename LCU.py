a8bc170ca48e561b1927ec9606fac6a1e67de11a
Lincoln Johnston <lajohnst@umich.edu>
2024-03-04 18:15:49 -0500

    implement F.V. SP3, need to verify that matrix equations were implemented correctly, and figure out why quantum solution isn't matching it as well as diffusion

--- LCU.py
+++ LCU.py
@@ -20,8 +20,8 @@ np.set_printoptions(threshold=np.inf)
 
 start = time.perf_counter()
 # number of variable points in each direction
-n_x = 32
-n_y = 32
+n_x = 16
+n_y = 16
 
 # number of points (including boundary values) in each direction
 n_pts_x = n_x + 2
@@ -31,23 +31,35 @@ n_pts_y = n_y + 2
 delta_x = 0.5
 delta_y = 0.5
 
-# create simple, constant boundary flux values for now, O(N)
-top_J_minus = 0
-bottom_J_minus = 0
-right_J_minus = 0
-left_J_minus = 0
-top_x_BCs = np.ones(n_pts_x) * top_J_minus
-bottom_x_BCs = np.zeros(n_pts_x) * bottom_J_minus
-right_y_BCs = np.zeros(n_pts_y) * right_J_minus
-left_y_BCs = np.zeros(n_pts_y) * left_J_minus
+# create boundary conditions
+top_I_1 = 0
+bottom_I_1 = 0
+right_I_1 = 0
+left_I_1 = 0
+top_x_I_1 = np.ones(n_pts_x) * top_I_1
+bottom_x_I_1 = np.zeros(n_pts_x) * bottom_I_1
+right_y_I_1 = np.zeros(n_pts_y) * right_I_1
+left_y_I_1 = np.zeros(n_pts_y) * left_I_1
+
+top_I_3 = 0
+bottom_I_3 = 0
+right_I_3 = 0
+left_I_3 = 0
+top_x_I_3 = np.ones(n_pts_x) * top_I_3
+bottom_x_I_3 = np.zeros(n_pts_x) * bottom_I_3
+right_y_I_3 = np.zeros(n_pts_y) * right_I_3
+left_y_I_3 = np.zeros(n_pts_y) * left_I_3
 
 # material data initialization, O(N)
-sigma_a = np.zeros(n_x*n_y)
+sigma_t = np.zeros(n_x*n_y)
+sigma_s0 = np.zeros(n_x*n_y)
+sigma_s2 = np.zeros(n_x*n_y)
 nu_sigma_f = np.zeros(n_x*n_y)
-D = np.zeros(n_x*n_y)
+D0 = np.zeros(n_x*n_y)
+D2 = np.zeros(n_x*n_y)
 Q = np.zeros(n_x*n_y)
 
-A_mat_size = (n_x) * (n_y)
+A_mat_size = 2 * (n_x) * (n_y)
 
 # from quantum state after passing through algorithm, extract solution to differential equation, O(2^(N+L))
 def get_solution_vector(solution, n):
@@ -67,18 +79,31 @@ def unroll_index(index_vec):
 def roll_index(index):
     return np.array([math.floor(index/n_y), index % n_y])
 
-# return flux at B.C.s outside of valid index range
-def get_BC_value(index):
+def get_I_1_value(index):
     i = index[0]
     j = index[1]
     if (i == 0):
-        return left_y_BCs[j+1]
+        return left_y_I_1[j]
     if (i == n_x-1):
-        return right_y_BCs[j+1]
+        return right_y_I_1[j]
     if (j == 0):
-        return bottom_x_BCs[i+1]
+        return bottom_x_I_1[i]
     if (j == n_y-1):
-        return top_x_BCs[i+1]
+        return top_x_I_1[i]
+    raise Exception("tried to get BC on non-boundary node")
+
+# return flux at B.C.s at edge of problem domain
+def get_I_3_value(index):
+    i = index[0]
+    j = index[1]
+    if (i == 0):
+        return left_y_I_3[j]
+    if (i == n_x-1):
+        return right_y_I_3[j]
+    if (j == 0):
+        return bottom_x_I_3[i]
+    if (j == n_y-1):
+        return top_x_I_3[i]
     raise Exception("tried to get BC on non-boundary node")
 
 # Set material data at each finite difference point, O(N)
@@ -97,26 +122,39 @@ def initialize_XSs():
             # fuel at center
             '''if (math.sqrt(x_val * x_val + y_val * y_val) < fuel_radius):
                 # use fuel XSs
-                sigma_a[i * n_y + j] = 4
+                sigma_t[i * n_y + j] = 5
+                sigma_s0[i * n_y + j] = 4
+                sigma_s2[i * n_y + j] = 0.1
                 nu_sigma_f[i * n_y + j] = 3
-                D[i * n_y + j] = 1
+                D0[i * n_y + j] = 1
+                D2[i * n_y + j] = 2
                 Q[i * n_y + j] = 5
             else:
                 # use moderator XSs
-                sigma_a[i * n_y + j] = 2
-                D[i * n_y + j] = 1'''
+                sigma_t[i * n_y + j] = 5
+                sigma_s0[i * n_y + j] = 5
+                sigma_s2[i * n_y + j] = 2
+                D0[i * n_y + j] = 1
+                D2[i * n_y + j] = 2'''
 
             # 4 fuel pins
             if (math.sqrt(math.pow(abs(x_val)-x_range/4,2) + math.pow(abs(y_val)-y_range/4,2)) < fuel_radius):
                 # use fuel XSs
-                sigma_a[i * n_y + j] = 4
-                nu_sigma_f[i * n_y + j] = 3
-                D[i * n_y + j] = 1
+
+                sigma_t[i * n_y + j] = 5
+                sigma_s0[i * n_y + j] = 4
+                sigma_s2[i * n_y + j] = 0.1
+                nu_sigma_f[i * n_y + j] = 0
+                D0[i * n_y + j] = 1
+                D2[i * n_y + j] = 2
                 Q[i * n_y + j] = 5
             else:
                 # use moderator XSs
-                sigma_a[i * n_y + j] = 2
-                D[i * n_y + j] = 1
+                sigma_t[i * n_y + j] = 5
+                sigma_s0[i * n_y + j] = 5
+                sigma_s2[i * n_y + j] = 2
+                D0[i * n_y + j] = 1
+                D2[i * n_y + j] = 2
 
 
 # Perform Gram-Schmidt orthogonalization to return V vector from LCU paper.
@@ -153,51 +191,193 @@ def is_unitary(matrix):
 # get averaged diffusion coefficient in either the "x" or "y" direction for interior points
 # lower_index is lower index in the direction of the averaged diffusion coefficient
 # set_index is the other dimension index
-def get_av_D(direction, lower_index, set_index):
+def get_av_D0(direction, lower_index, set_index):
     if direction == "x":
-        D_lower = D[unroll_index([lower_index, set_index])]
-        D_upper = D[unroll_index([lower_index+1, set_index])]
+        D_lower = D0[unroll_index([lower_index, set_index])]
+        D_upper = D0[unroll_index([lower_index+1, set_index])]
         delta = delta_x
     elif direction == "y":
-        D_lower = D[unroll_index([set_index, lower_index])]
-        D_upper = D[unroll_index([set_index, lower_index+1])]
+        D_lower = D0[unroll_index([set_index, lower_index])]
+        D_upper = D0[unroll_index([set_index, lower_index+1])]
         delta = delta_y
     return 2 * (D_lower/delta) * (D_upper/delta) / (D_lower/delta + D_upper/delta)
 
-def get_edge_D(beta, x_i, y_i, delta):
-    return 2 * (beta/2) * (D[unroll_index([x_i, y_i])]/delta) / (beta/2 + (D[unroll_index([x_i, y_i])]/delta))
+def get_av_D2(direction, lower_index, set_index):
+    if direction == "x":
+        D_lower = D2[unroll_index([lower_index, set_index])]
+        D_upper = D2[unroll_index([lower_index+1, set_index])]
+        delta = delta_x
+    elif direction == "y":
+        D_lower = D2[unroll_index([set_index, lower_index])]
+        D_upper = D2[unroll_index([set_index, lower_index+1])]
+        delta = delta_y
+    return 2 * (D_lower/delta) * (D_upper/delta) / (D_lower/delta + D_upper/delta)
+
+#def get_edge_D(beta, x_i, y_i, delta):
+#    return 2 * (beta/2) * (D[unroll_index([x_i, y_i])]/delta) / (beta/2 + (D[unroll_index([x_i, y_i])]/delta))
 
 
 
 # use finite volume method to contruct the A matrix reprenting the diffusion equation in the form Ax=b, O(N)
 def construct_A_matrix():
     fd_order = 2
+    beta = 0.5
+    phi_2_offset = n_x * n_y
     A_matrix = np.zeros((A_mat_size, A_mat_size))
+    b_vector = np.zeros((A_mat_size))
     for x_i in range(n_x):
         for y_i in range(n_y):
             i = unroll_index([x_i, y_i])
             if(x_i == 0): # left BC, normal vector = (-1,0)
-                J_x_minus = get_edge_D(0.5, x_i ,y_i,delta_x) * delta_y
+                # these coefficients will be the same for all cells with the same material and mesh size so
+                # need to imporove efficiency of this by storing these values beforehand instead of recalculating for each B.C. cell
+                a1 = (1 + 4 * D0[unroll_index([x_i, y_i])]/delta_x)
+                a2 = (-3/4) * D0[unroll_index([x_i, y_i])]/D2[unroll_index([x_i, y_i])]
+                a3 = 2 * D0[unroll_index([x_i, y_i])]/delta_x
+                a4 = (-3/4) * 2 * D0[unroll_index([x_i, y_i])] / delta_x
+                a5 =  4 * D0[unroll_index([x_i, y_i])]/delta_x * 2 * get_I_1_value([x_i,y_i])
+
+                b2 = (1 + (80/21) * D2[unroll_index([x_i, y_i])]/delta_x)
+                b1 = (-1/7) * D2[unroll_index([x_i, y_i])]/D0[unroll_index([x_i, y_i])]
+                b4 = 2 * D2[unroll_index([x_i, y_i])]/delta_x
+                b3 = (-2/7) * D2[unroll_index([x_i, y_i])] / delta_x
+                b5 =  (6/5) * (80/21) * D2[unroll_index([x_i, y_i])]/delta_x * get_I_3_value([x_i,y_i])
+
+                denom = (a1 - a2 * b1 / b2)
+                c1 = (a5 - a2 * b5 / b2) / denom
+                c2 = (a2 * b3 / b2 - a3) / denom
+                c3 = (a2 * b4 / b2 - a4) / denom
+
+                b_vector[i] += c1 * delta_y
+                A_matrix[i,i] -= c2 * delta_y
+                A_matrix[i,i+phi_2_offset] -= c3 * delta_y
+
+                b_vector[i + phi_2_offset] += (b5 - b1 * c1) / b2 * delta_y
+                A_matrix[i+phi_2_offset,i] -= (-b1 * c2 - b3) / b2 * delta_y
+                A_matrix[i+phi_2_offset,i+phi_2_offset] -= (-b1 * c3 - b4) / b2 * delta_y
             else:
-                J_x_minus = get_av_D("x",x_i-1,y_i) * delta_y
-                A_matrix[i,unroll_index([x_i-1, y_i])] =  -J_x_minus # (i-1,j) terms
+                # Phi_0 equations
+                x_minus_term_0 = get_av_D0("x",x_i-1,y_i) * delta_y
+                A_matrix[i,unroll_index([x_i-1, y_i])] =  -x_minus_term_0 # phi_0, (i-1,j) term
+                A_matrix[i,i] +=  x_minus_term_0 # phi_0, (i,j) term
+
+                # Phi_2 equations
+                x_minus_term_2 = get_av_D2("x",x_i-1,y_i) * delta_y
+                A_matrix[i+phi_2_offset,unroll_index([x_i-1, y_i]) + phi_2_offset] =  -x_minus_term_2 # phi_2, (i-1,j) term
+                A_matrix[i+phi_2_offset,i+phi_2_offset] +=  x_minus_term_2 # phi_2, (i,j) term
             if(x_i == n_x - 1): # right BC, normal vector = (1,0)
-                J_x_plus = get_edge_D(0.5, x_i ,y_i,delta_x) * delta_y
+                d1 = (-1 - 4 * D0[unroll_index([x_i, y_i])]/delta_x)
+                d2 = (3/4) * D0[unroll_index([x_i, y_i])]/D2[unroll_index([x_i, y_i])]
+                d3 = 2 * D0[unroll_index([x_i, y_i])]/delta_x
+                d4 = (-3/4) * 2 * D0[unroll_index([x_i, y_i])] / delta_x
+                d5 =  4 * D0[unroll_index([x_i, y_i])]/delta_x * 2 * get_I_1_value([x_i,y_i])
+
+                e2 = (-1 - (80/21) * D2[unroll_index([x_i, y_i])]/delta_x)
+                e1 = (1/7) * D2[unroll_index([x_i, y_i])]/D0[unroll_index([x_i, y_i])]
+                e4 = 2 * D2[unroll_index([x_i, y_i])]/delta_x
+                e3 = (-2/7) * D2[unroll_index([x_i, y_i])] / delta_x
+                e5 =  (6/5) * (80/21) * D2[unroll_index([x_i, y_i])]/delta_x * get_I_3_value([x_i,y_i])
+
+                denom = (d1 - d2 * e1 / e2)
+                f1 = (d5 - d2 * e5 / e2) / denom
+                f2 = (d2 * e3 / e2 - d3) / denom
+                f3 = (d2 * e4 / e2 - d4) / denom
+
+                b_vector[i] -= f1 * delta_y
+                A_matrix[i,i] += f2 * delta_y
+                A_matrix[i,i+phi_2_offset] += f3 * delta_y
+
+                b_vector[i + phi_2_offset] -= (e5 - e1 * f1) / e2 * delta_y
+                A_matrix[i+phi_2_offset,i] += (-e1 * f2 - e3) / e2 * delta_y
+                A_matrix[i+phi_2_offset,i+phi_2_offset] += (-e1 * f3 - e4) / e2 * delta_y
             else:
-                J_x_plus = get_av_D("x",x_i,y_i) * delta_y
-                A_matrix[i,unroll_index([x_i+1, y_i])] =  -J_x_plus # (i+1,j) terms
+                # Phi_0 equations
+                x_plus_term_0 = get_av_D0("x",x_i,y_i) * delta_y
+                A_matrix[i,unroll_index([x_i+1, y_i])] =  -x_plus_term_0 # phi_0, (i+1,j) term
+                A_matrix[i,i] +=  x_plus_term_0 # phi_0, (i,j) term
+
+                # Phi_2 equations
+                x_plus_term_2 = get_av_D2("x",x_i,y_i) * delta_y
+                A_matrix[i+phi_2_offset,unroll_index([x_i+1, y_i]) + phi_2_offset] =  -x_plus_term_2 # phi_2, (i+1,j) term
+                A_matrix[i+phi_2_offset,i+phi_2_offset] +=  x_plus_term_2 # phi_2, (i,j) term
             if(y_i == 0): # bottom BC, normal vector = (0,-1)
-                J_y_minus = get_edge_D(0.5, x_i ,y_i,delta_y) * delta_x
+                a1 = (1 + 4 * D0[unroll_index([x_i, y_i])]/delta_y)
+                a2 = (-3/4) * D0[unroll_index([x_i, y_i])]/D2[unroll_index([x_i, y_i])]
+                a3 = 2 * D0[unroll_index([x_i, y_i])]/delta_y
+                a4 = (-3/4) * 2 * D0[unroll_index([x_i, y_i])] / delta_y
+                a5 =  4 * D0[unroll_index([x_i, y_i])]/delta_y * 2 * get_I_1_value([x_i,y_i])
+
+                b2 = (1 + (80/21) * D2[unroll_index([x_i, y_i])]/delta_y)
+                b1 = (-1/7) * D2[unroll_index([x_i, y_i])]/D0[unroll_index([x_i, y_i])]
+                b4 = 2 * D2[unroll_index([x_i, y_i])]/delta_y
+                b3 = (-2/7) * D2[unroll_index([x_i, y_i])] / delta_y
+                b5 =  (6/5) * (80/21) * D2[unroll_index([x_i, y_i])]/delta_y * 2 * get_I_3_value([x_i,y_i])
+
+                denom = (a1 - a2 * b1 / b2)
+                c1 = (a5 - a2 * b5 / b2) / denom
+                c2 = (a2 * b3 / b2 - a3) / denom
+                c3 = (a2 * b4 / b2 - a4) / denom
+
+                b_vector[i] += c1 * delta_x
+                A_matrix[i,i] -= c2 * delta_x
+                A_matrix[i,i+phi_2_offset] -= c3 * delta_x
+
+                b_vector[i + phi_2_offset] += (b5 - b1 * c1) / b2 * delta_x
+                A_matrix[i+phi_2_offset,i] -= (-b1 * c2 - b3) / b2 * delta_x
+                A_matrix[i+phi_2_offset,i+phi_2_offset] -= (-b1 * c3 - b4) / b2 * delta_x
             else:
-                J_y_minus = get_av_D("y",y_i-1,x_i) * delta_x
-                A_matrix[i,unroll_index([x_i, y_i-1])] =  -J_y_minus # (i,j-1) terms
+                # Phi_0 equations
+                y_minus_term_0 = get_av_D0("y",x_i,y_i-1) * delta_x
+                A_matrix[i,unroll_index([x_i, y_i-1])] =  -y_minus_term_0 # phi_0, (i,j-1) term
+                A_matrix[i,i] +=  y_minus_term_0 # phi_0, (i,j) term
+
+                # Phi_2 equations
+                y_minus_term_2 = get_av_D2("y",x_i,y_i-1) * delta_x
+                A_matrix[i+phi_2_offset,unroll_index([x_i, y_i-1]) + phi_2_offset] =  -y_minus_term_2 # phi_2, (i,j-1) term
+                A_matrix[i+phi_2_offset,i+phi_2_offset] +=  y_minus_term_2 # phi_2, (i,j) term
             if(y_i == n_y - 1): # right BC, normal vector = (0,1)
-                J_y_plus = get_edge_D(0.5, x_i ,y_i,delta_y) * delta_x
+                d1 = (-1 - 4 * D0[unroll_index([x_i, y_i])]/delta_y)
+                d2 = (3/4) * D0[unroll_index([x_i, y_i])]/D2[unroll_index([x_i, y_i])]
+                d3 = 2 * D0[unroll_index([x_i, y_i])]/delta_y
+                d4 = (-3/4) * 2 * D0[unroll_index([x_i, y_i])] / delta_y
+                d5 =  4 * D0[unroll_index([x_i, y_i])]/delta_y * 2 * get_I_1_value([x_i,y_i])
+
+                e2 = (-1 - (80/21) * D2[unroll_index([x_i, y_i])]/delta_y)
+                e1 = (1/7) * D2[unroll_index([x_i, y_i])]/D0[unroll_index([x_i, y_i])]
+                e4 = 2 * D2[unroll_index([x_i, y_i])]/delta_y
+                e3 = (-2/7) * D2[unroll_index([x_i, y_i])] / delta_y
+                e5 =  (6/5) * (80/21) * D2[unroll_index([x_i, y_i])]/delta_y * get_I_3_value([x_i,y_i])
+
+                denom = (d1 - d2 * e1 / e2)
+                f1 = (d5 - d2 * e5 / e2) / denom
+                f2 = (d2 * e3 / e2 - d3) / denom
+                f3 = (d2 * e4 / e2 - d4) / denom
+
+                b_vector[i] -= f1 * delta_x
+                A_matrix[i,i] += f2 * delta_x
+                A_matrix[i,i+phi_2_offset] += f3 * delta_x
+
+                b_vector[i + phi_2_offset] -= (e5 - e1 * f1) / e2 * delta_x
+                A_matrix[i+phi_2_offset,i] += (-e1 * f2 - e3) / e2 * delta_x
+                A_matrix[i+phi_2_offset,i+phi_2_offset] += (-e1 * f3 - e4) / e2 * delta_x
             else:
-                J_y_plus = get_av_D("x",y_i,x_i) * delta_x
-                A_matrix[i,unroll_index([x_i, y_i+1])] =  -J_y_plus # (i,j+1) terms
-            A_matrix[i,i] = J_x_minus + J_x_plus + J_y_minus + J_y_plus + (sigma_a[i] - nu_sigma_f[i]) * delta_x * delta_y
-    return A_matrix
+                # Phi_0 equations
+                y_plus_term_0 = get_av_D0("y",x_i,y_i) * delta_x
+                A_matrix[i,unroll_index([x_i, y_i+1])] =  -y_plus_term_0 # phi_0, (i,j+1) term
+                A_matrix[i,i] +=  y_plus_term_0 # phi_0, (i,j) term
+
+                # Phi_2 equations
+                y_plus_term_2 = get_av_D2("y",x_i,y_i) * delta_x
+                A_matrix[i+phi_2_offset,unroll_index([x_i, y_i+1]) + phi_2_offset] =  -y_plus_term_2 # phi_2, (i,j+1) term
+                A_matrix[i+phi_2_offset,i+phi_2_offset] +=  y_plus_term_2 # phi_2, (i,j) term
+            A_matrix[i,i] += (sigma_t[i] - nu_sigma_f[i] - sigma_s0[i]) * delta_x * delta_y
+            A_matrix[i,i + phi_2_offset] += (-2) * (sigma_t[i] - nu_sigma_f[i] - sigma_s0[i]) * delta_x * delta_y
+            b_vector[i] += Q[i] * delta_x * delta_y
+
+            A_matrix[i+phi_2_offset,i] += (-2/5) * (sigma_t[i] - nu_sigma_f[i] - sigma_s0[i]) * delta_x * delta_y
+            A_matrix[i+phi_2_offset,i + phi_2_offset] += ((sigma_t[i] - nu_sigma_f[i] - sigma_s2[i]) + (4/5) * (sigma_t[i] - nu_sigma_f[i] - sigma_s0[i])) * delta_x * delta_y
+            b_vector[i+phi_2_offset] += (-2/5) * Q[i] * delta_x * delta_y
+    return A_matrix, b_vector
 
 # return gate to transform 0 state to vector b represented as a quantum state
 def get_b_setup_gate(vector, nb):
@@ -253,14 +433,11 @@ def get_fourier_unitaries(J, K, y_max, z_max, matrix, doFullSolution):
 
 # make A matrix
 initialize_XSs() # create the vectors holding the material data at each discretized point
-A_matrix = construct_A_matrix() # use the material data (like XSs) to make the A matrix for the equation being solved
+A_matrix, b_vector = construct_A_matrix() # use the material data (like XSs) to make the A matrix for the equation being solved
 
-# make b vector
-b_vector = Q * delta_x * delta_y
-
-#print("A matrix:")
-#print(A_matrix)
-'''print("\n b vector: ")
+'''print("A matrix:")
+print(A_matrix)
+print("\n b vector: ")
 print(b_vector)
 eigenvalues, eigenvectors = np.linalg.eig(A_matrix)
 print("A eigenvalues: ", eigenvalues)
@@ -270,7 +447,7 @@ material_initialization_time = time.perf_counter()
 print("Initialization Time: ", material_initialization_time - start)
 
 # Do LCU routine (https://arxiv.org/pdf/1511.02306.pdf), equation 18
-num_LCU_bits = 3
+num_LCU_bits = 4
 num_unitaries = pow(2,num_LCU_bits)
 last_error_norm = np.inf
 
@@ -398,6 +575,14 @@ state_vec = job_result.get_statevector(qc).data
 #print(state_vec[0:A_mat_size])
 state_vec = np.real(state_vec[len(quantum_b_vector) - len(b_vector):len(quantum_b_vector)])
 classical_sol_vec = np.linalg.solve(A_matrix, b_vector)
+print(classical_sol_vec)
+
+# find scalar flux value from 0th and 2nd moments in SP3 equations
+for i in range(int(len(state_vec)/2)):
+    state_vec[i] -= 2 * state_vec[int(i + len(state_vec)/2)]
+    classical_sol_vec[i] -= 2 * classical_sol_vec[int(i + len(classical_sol_vec)/2)]
+state_vec = state_vec[:int(len(state_vec)/2)]
+classical_sol_vec = classical_sol_vec[:int(len(classical_sol_vec)/2)]
 
 state_vec = state_vec * np.linalg.norm(classical_sol_vec) / np.linalg.norm(state_vec) # scale result to match true answer
 
