a3eceea43f26f5c5d4b92d194dda4dde4e18f630
Lincoln Johnston <lajohnst@umich.edu>
2025-01-30 13:54:51 -0500

    add some timers, run the really inefficient function I made fewer times so it goes faster

--- QPE-test-numpy.py
+++ QPE-test-numpy.py
@@ -65,15 +65,7 @@ def get_IQFT_matrix(n_bits):
     return (1/math.sqrt(mat_size)) * final_mat.conj()
 
 
-
-'''n_qubits = 4  # Total number of qubits
-new_order = [1, 3, 0, 2]  # Qubits the unitary is acting on
-unitary_2q = np.array([[1, 0, 0, 0], 
-                       [0, 0, 1, 0], 
-                       [0, 1, 0, 0], 
-                       [0, 0, 0, 1]])  # An example of a two-qubit unitary (like CNOT with swapped control-target)
-
-full_unitary = apply_unitary_to_qubits(n_qubits, new_order, unitary_2q)'''
+last_time = time.time()
 
 #sim_path = 'simulations/LCU_8G_diffusion/'
 sim_path = 'simulations/test_1G_diffusion/'
@@ -116,11 +108,17 @@ n_states = A_mat_size * n_eig_eval_states
 state_vec = np.zeros(n_states, dtype=np.complex_)
 state_vec[0] = 1
 
+print("setup time: ", time.time() - last_time)
+last_time = time.time()
+
 # put the eigenvector register in the phi_0 state
 state_prep_unitary = np.zeros((A_mat_size,A_mat_size))
 state_prep_unitary[:,0] = eigenvector_input
 state_prep_unitary = np.kron(state_prep_unitary, np.eye(n_eig_eval_states))
 state_vec = state_prep_unitary @ state_vec
+
+print("state prep time: ", time.time() - last_time)
+last_time = time.time()
 
 #A_times_eigenvector = A_matrix @ (eigenvector_input) / A_mat_size
 num_iter = 1000000
@@ -129,8 +127,11 @@ num_iter = 1000000
 state_vec = np.kron(sqrtB,np.eye(n_eig_eval_states)) @ state_vec
 state_vec = state_vec / np.linalg.norm(state_vec)
 
+print("sqrtB time: ", time.time() - last_time)
+last_time = time.time()
+
 # do QPE on A_squiggle_pow
-# apply tht Hadamards
+# apply the Hadamards
 H = 1/math.sqrt(2) * np.array([[1, 1],[1,-1]])
 combined_hadamard_gate = 1
 for i in range(n_eig_eval_bits):
@@ -138,24 +139,41 @@ for i in range(n_eig_eval_bits):
 full_hadamard_unitary = apply_unitary_to_qubits(n_bits, list(range(A_bits, A_bits + n_eig_eval_bits)) + list(range(A_bits)), combined_hadamard_gate)
 state_vec = full_hadamard_unitary @ state_vec
 
+print("Hadamard time: ", time.time() - last_time)
+last_time = time.time()
+
 # apply the controlled unitaries
 control_A_squiggle_pow = np.kron(np.array([[1,0],[0,0]]), np.eye(A_mat_size)) + np.kron(np.array([[0,0],[0,1]]), A_squiggle_pow)
 for c_i in range(n_bits-1,A_bits-1,-1):
-    for j in range(int(math.pow(2,n_bits-c_i-1))):
-        full_control_unitary = apply_unitary_to_qubits(n_bits, [c_i] + list(range(A_bits)) + list(range(A_bits, c_i)) + list(range(c_i + 1, n_bits)), control_A_squiggle_pow)
-        state_vec = full_control_unitary @ state_vec
+    # apply controlled unitaries one at a time (how we are going to actually do it in a quantum computer)
+    #for j in range(int(math.pow(2,n_bits-c_i-1))):
+    #    full_control_unitary = apply_unitary_to_qubits(n_bits, [c_i] + list(range(A_bits)) + list(range(A_bits, c_i)) + list(range(c_i + 1, n_bits)), control_A_squiggle_pow)
+    #    state_vec = full_control_unitary @ state_vec
+    # apply the power of the unitaries as a unitary itself
+    full_control_unitary = apply_unitary_to_qubits(n_bits, [c_i] + list(range(A_bits)) + list(range(A_bits, c_i)) + list(range(c_i + 1, n_bits)), np.linalg.matrix_power(control_A_squiggle_pow, int(math.pow(2,n_bits-c_i-1))))
+    state_vec = full_control_unitary @ state_vec
+
+print("controlled unitary time: ", time.time() - last_time)
+last_time = time.time()
 
 # do the IQFT
+test = get_IQFT_matrix(3)
 IQFT_mat = get_IQFT_matrix(n_eig_eval_bits)
-#full_iqft_unitary = apply_unitary_to_qubits(n_bits, list(range(A_bits, A_bits + n_eig_eval_bits)) + list(range(A_bits)), IQFT_mat)
-full_iqft_unitary = apply_unitary_to_qubits(n_bits, list(range(A_bits + n_eig_eval_bits - 1, A_bits - 1, -1)) + list(range(A_bits)), IQFT_mat)
+full_iqft_unitary = apply_unitary_to_qubits(n_bits, list(range(A_bits, A_bits + n_eig_eval_bits)) + list(range(A_bits)), IQFT_mat)
+#full_iqft_unitary = apply_unitary_to_qubits(n_bits, list(range(A_bits + n_eig_eval_bits - 1, A_bits - 1, -1)) + list(range(A_bits)), IQFT_mat)
 state_vec = full_iqft_unitary @ state_vec
+
+print("IQFT time: ", time.time() - last_time)
+last_time = time.time()
 
 
 # do B^(-1/2) on the eigenvector state to return it to its original state
 state_vec = np.kron(sqrtB_inv,np.eye(n_eig_eval_states)) @ state_vec
 state_vec = state_vec / np.linalg.norm(state_vec)
 print("circuits made")
+
+print("ssqrtB_inv time: ", time.time() - last_time)
+last_time = time.time()
 
 state_vec_collapsed = np.zeros(n_eig_eval_states)
 for i in range(n_eig_eval_states):
@@ -164,5 +182,5 @@ for i in range(n_eig_eval_states):
         #print('{:b}'.format(i).zfill(A_bits), "    ",'{:b}'.format(j).zfill(n_eig_eval_bits), "    ", round(state_vec[i*int(math.pow(2,A_bits)) + j],8))
     state_vec_collapsed[i] = math.sqrt(state_vec_collapsed[i])
 index_max = max(range(len(state_vec_collapsed)), key=state_vec_collapsed.__getitem__)
-print("eigenvalue found: ", (index_max-1) / n_eig_eval_states)
+print("eigenvalue found: ", (index_max) / n_eig_eval_states)
 print("expected eigenvalue: ", eigvals[eig_index])
