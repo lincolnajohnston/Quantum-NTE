ae16d631673aed59cee4b444ee98c838ecc07e16
Lincoln Johnston <lajohnst@umich.edu>
2025-04-15 09:03:50 -0400

    add in finite difference with spatially varying diffusion coefficient to ProblemData.py, run a test on it in ProblemData-scaling.py

--- ProblemData-scaling.py
+++ ProblemData-scaling.py
@@ -10,7 +10,7 @@ import math
 # Equation Diffusion
 # Geometry: Square Fuel Pin
 # Notes: 
-n_dim = 1
+'''n_dim = 1
 input_folder = 'simulations/ProblemData_1D_scaling_tests_fuel_pin/'
 min_qubits = 1
 max_qubits = 10
@@ -94,7 +94,7 @@ eigenvalue_error = eigenvalue_results - closest_eig
 #eigenvector_error = eigenvector_results - closest_eigenvector
 eigenvector_diffs = [eigenvector_results[i,:] - closest_eigenvector for i in range(max_qubits - min_qubits + 1)]
 eigenvector_l2_norm = np.array([np.linalg.norm(eigenvector_results[i,:] - closest_eigenvector) for i in range(max_qubits - min_qubits + 1)])
-eigenvector_linf_norm = np.array([np.linalg.norm(eigenvector_results[i,:] - closest_eigenvector, ord=np.inf) for i in range(max_qubits - min_qubits + 1)])
+eigenvector_linf_norm = np.array([np.linalg.norm(eigenvector_results[i,:] - closest_eigenvector, ord=np.inf) for i in range(max_qubits - min_qubits + 1)])'''
 
 
 ############# 2D Diffusion eigenvalue results ###############
@@ -222,7 +222,7 @@ eigenvector_linf_norm = np.array([np.linalg.norm(eigenvector_results[i,:] - clos
 # Dimensions: 1
 # Equation Diffusion
 # Geometry: Homogeneous Fuel
-# Notes: shows h^2 dependence of eigenvalue error, same as the local truncation error
+# Notes: shows h^2 dependence of eigenvalue error, same as the local truncation error, h dependence for the L2 of the eigenvector
 '''input_folder = 'simulations/ProblemData_1D_scaling_tests/'
 n_dim = 1
 min_qubits = 1
@@ -244,7 +244,7 @@ for i in range(min_qubits, max_qubits + 1):
     A_matrix, B_matrix = data.diffusion_construct_L_F_matrices(A_mat_size)
     eigvals, eigvecs = eigh(A_matrix, B_matrix, eigvals_only=False)
     eigenvalue_results[i - min_qubits] = eigvals[0]
-    eigenvector_results[i - min_qubits,:] = np.kron(eigvecs[:,0].reshape(tuple([int(math.pow(2,i)) for d in range(n_dim)])),np.ones(tuple([int(max_dim_size/int(math.pow(2,i))) for d in range(n_dim)]))).flatten() * eigvecs[0,0] / abs(eigvecs[0,0]) # scale eigenvector and make first value positive
+    eigenvector_results[i - min_qubits,:] = np.kron(eigvecs[:,0].reshape(tuple([int(math.pow(2,i)) for d in range(n_dim)])),np.ones(tuple([int(max_dim_size/int(math.pow(2,i))) for d in range(n_dim)]))).flatten() * eigvecs[0,0] / abs(eigvecs[0,0]) # extend/interpolate eigenvectors onto finest grid and make first value positive
 
 closest_eig = eigenvalue_results[-1]
 closest_eigenvector = eigenvector_results[-1,:]
@@ -254,6 +254,46 @@ eigenvalue_error = eigenvalue_results - closest_eig
 eigenvector_diffs = [eigenvector_results[i,:] - closest_eigenvector for i in range(max_qubits - min_qubits + 1)]
 eigenvector_l2_norm = np.array([np.linalg.norm(eigenvector_results[i,:] - closest_eigenvector) for i in range(max_qubits - min_qubits + 1)])
 eigenvector_linf_norm = np.array([np.linalg.norm(eigenvector_results[i,:] - closest_eigenvector, ord=np.inf) for i in range(max_qubits - min_qubits + 1)])'''
+
+
+############# 1D Diffusion FD eigenvalue results ###############
+# Dimensions: 1
+# Equation Diffusion
+# Geometry: Homogeneous Fuel
+# Discretization Scheme: Finite Difference
+# Notes: 
+input_folder = 'simulations/ProblemData_1D_scaling_tests/'
+n_dim = 1
+min_qubits = 1
+max_qubits = 10
+max_dim_size = int(math.pow(2,max_qubits))
+x_range = 4.0
+#input_files = ['input-N=' + str(int(math.pow(2,i))) + '.txt' for i in range(1,max_qubits + 1)]
+input_file = 'input.txt'
+
+eigenvalue_results = np.zeros(max_qubits - min_qubits + 1)
+eigenvector_results = np.zeros((max_qubits - min_qubits + 1, int(math.pow(max_dim_size,n_dim))))
+data = ProblemData.ProblemData(input_folder + input_file)
+for i in range(min_qubits, max_qubits + 1):
+    data.n = np.array([int(math.pow(2,i))] * n_dim)
+    data.h = x_range / (data.n + 1)
+    data.initialize_BC()
+    data.initialize_geometry()
+    A_mat_size = math.prod(data.n) * data.G
+    A_matrix, B_matrix = data.diffusion_FD_construct_L_F_matrices()
+    eigvals, eigvecs = eigh(A_matrix, B_matrix, eigvals_only=False)
+    eigenvalue_results[i - min_qubits] = eigvals[0]
+    fund_eigvec = np.kron(eigvecs[:,0].reshape(tuple([int(math.pow(2,i)) for d in range(n_dim)])),np.ones(tuple([int(max_dim_size/int(math.pow(2,i))) for d in range(n_dim)]))).flatten() * eigvecs[0,0] / abs(eigvecs[0,0]) # extend/interpolate eigenvectors onto finest grid and make first value positive
+    eigenvector_results[i - min_qubits,:] = fund_eigvec / np.linalg.norm(fund_eigvec)
+
+closest_eig = eigenvalue_results[-1]
+closest_eigenvector = eigenvector_results[-1,:]
+
+eigenvalue_error = eigenvalue_results - closest_eig
+#eigenvector_error = eigenvector_results - closest_eigenvector
+eigenvector_diffs = [eigenvector_results[i,:] - closest_eigenvector for i in range(max_qubits - min_qubits + 1)]
+eigenvector_l2_norm = np.array([np.linalg.norm(eigenvector_results[i,:] - closest_eigenvector) for i in range(max_qubits - min_qubits + 1)])
+eigenvector_linf_norm = np.array([np.linalg.norm(eigenvector_results[i,:] - closest_eigenvector, ord=np.inf) for i in range(max_qubits - min_qubits + 1)])
 
 
 
@@ -304,7 +344,7 @@ plt.figure()
 
 
 # plot condition numbers
-plt.loglog(1/np.power(2,list(range(max_qubits)))[:-1], abs(condAs[:-1]), '-o')
+'''plt.loglog(1/np.power(2,list(range(max_qubits)))[:-1], abs(condAs[:-1]), '-o')
 plt.xlabel("h")
 plt.ylabel("A condition number")
 plt.figure()
@@ -334,7 +374,7 @@ plt.figure()
 plt.loglog(1/np.power(2,list(range(max_qubits)))[:-1], abs(B_min_sings[:-1]), '-o')
 plt.xlabel("h")
 plt.ylabel("B_inv_norm")
-plt.figure()
+plt.figure()'''
 
 
 
